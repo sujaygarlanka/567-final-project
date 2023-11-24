6358c1f0448f85352c67108fa5a4a5fb531cb19f
Sujay Garlanka <sujay.garlanka@gmail.com>
2023-11-23 23:10:43 -0800

    wip

---
+++ code/lib/computational_graph_approach/utils.py
@@ -0,0 +1,19 @@
+import time
+import numpy as np
+
+class Timer():
+    def __init__(self):
+        self.start_time = time.time()
+
+    def start(self):
+        self.start_time = time.time()
+
+    def stop(self, stdout=None):
+        elapsed =  time.time() - self.start_time
+        if stdout is not None:
+            print(f"{stdout} | {elapsed}")
+        else:
+            return elapsed
+        
+def get_scalar_values(x):
+    return np.array([o.val for o in x.flatten()]).reshape(x.shape)
