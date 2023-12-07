7f603a6e79e4c54d2225d0bf690d3765a149fdb8
Sujay Garlanka <sujay.garlanka@gmail.com>
2023-12-07 15:24:50 -0800

    clean up code

--- code/lib/matrix_approach/layers.py
+++ code/lib/matrix_approach/layers.py
@@ -60,6 +60,7 @@ class FC():
     
     def backward(self, prev_grad):    
         # Calculate gradients for weights
+
         # 3D matrix of shape (batch_size, num_outputs, num_inputs + 1)
         input_matrix = np.expand_dims(self.inputs, axis=1)
         input_matrix = np.repeat(input_matrix, self.params_shape[1], axis=1)
@@ -93,10 +94,76 @@ class Conv2D():
         self.kernel_size = kernel_size
         self.number_filters = number_filters
         self.params = np.array([random.uniform(-1, 1) for _ in range(number_filters * (kernel_size * kernel_size * input_channels + 1))]).reshape(number_filters, -1)
+        self.grads = np.empty(self.params.shape)
         self.stride = stride
         self.padding = padding
         self.params_shape = (number_filters, kernel_size, kernel_size, input_channels)
+        self.trainable = True
+
+        # Variables during forward pass to be used for the backward pass
         self.padded_input = None
+        self.sub_matrices = None
+
+    def __call__(self, inputs):
+        self.padded_input = np.pad(inputs, ((0,0),(self.padding,self.padding),(self.padding,self.padding),(0,0)), 'constant', constant_values=0)
+        self.sub_matrices = self._get_conv_sub_matrices(self.padded_input)
+
+        batch_size, output_height, output_width, num_filters = self._get_output_shape(self.padded_input)
+        output = None 
+        for p in self.params:
+            weights = p[:-1]
+            bias = p[-1]
+            weights = weights.reshape(self.kernel_size, self.kernel_size, self.input_channels)
+            convolved = np.tensordot(self.sub_matrices, weights, axes=([2, 3, 4], [0, 1, 2]))
+            convolved += bias
+            convolved = convolved.reshape(batch_size, output_height, output_width, 1)
+            output = convolved if output is None else np.concatenate([output, convolved], axis=-1)
+        return output
+
+    def backward(self, prev_grad):
+        # Calculate gradients for weights by iterating over the filters
+        ###############################################################
+        for i, p in enumerate(self.params):
+            # Get gradients for outputs associated with the current filter
+            prev_grad_filter = prev_grad[:, :, :, i]
+            prev_grad_filter = prev_grad_filter.reshape(prev_grad_filter.shape[0], prev_grad_filter.shape[1] * prev_grad_filter.shape[2])
+            # Get gradients for weights by multiplying the inputs by the gradients of the outputs
+            weights_grads = np.einsum('ijklm,ij->ijklm', self.sub_matrices, prev_grad_filter)
+            # Sum the gradients across the batch
+            weights_grads = np.reshape(weights_grads, (weights_grads.shape[0]*weights_grads.shape[1], -1))
+            weights_grads = np.sum(weights_grads, axis=0)
+            # Get the gradient for the bias by summing the gradients of the outputs across the batch
+            bias_grad = np.sum(prev_grad_filter.flatten())   
+            # Update the weights and bias
+            self.grads[i] = np.append(weights_grads, bias_grad)
+
+        # Calculate gradients for inputs by iterating over the filters
+        ###############################################################
+        batch_size, output_height, output_width, num_filters = self._get_output_shape(self.padded_input)
+        filters = self.params[:, :-1].reshape(self.params_shape)
+        filter_masks = []
+        
+        input_num_rows = self.padded_input.shape[1]
+        input_num_cols = self.padded_input.shape[2]
+
+        for i in range(output_height):
+            for j in range(output_width):
+                curr_filter_mask = np.pad(filters, ((0,0),(i * self.stride, input_num_rows - (i*self.stride + self.kernel_size)),(j * self.stride, input_num_cols - (j*self.stride + self.kernel_size)),(0,0)), 'constant', constant_values=0)
+                filter_masks.append(curr_filter_mask)
+
+        # (output_width * output_height, num_filters, input_rows, input_cols, input_channels)
+        filter_masks = np.array(filter_masks)
+        # (1, output_width * output_height, num_filters, input_rows, input_cols, input_channels)
+        filter_masks = np.expand_dims(filter_masks, axis=0)
+        # (batch_size, output_width * output_height, num_filters, input_rows, input_cols, input_channels)
+        filter_masks = np.repeat(filter_masks, batch_size, axis=0)
+        # (batch_size, output_width * output_height, num_filters)
+        prev_grad_shaped = prev_grad.reshape(prev_grad.shape[0], prev_grad.shape[1]*prev_grad.shape[2], prev_grad.shape[3])
+        # (batch_size, output_width * output_height, num_filters, input_rows, input_cols, input_channels)
+        # ** Need to trim if there is padding **
+        backward_grads = np.einsum('ijk,ijklmn->ijklmn', prev_grad_shaped, filter_masks)
+        # Sum for gradients
+        return np.sum(backward_grads, axis=(1,2))
 
     def _get_output_shape(self, inputs):
         batch_size = inputs.shape[0]
@@ -105,45 +172,18 @@ class Conv2D():
         output_width = int((input_width + 2*self.padding - self.kernel_size) / self.stride + 1)
         output_height = int((input_height + 2*self.padding - self.kernel_size) / self.stride + 1)
         return batch_size, output_height, output_width, self.number_filters
-
-    def forward(self, inputs):
-        self.padded_input = np.pad(inputs, ((0,0),(1,1),(1,1),(0,0)), 'constant', constant_values=0)
-        sub_matrices = self._get_conv_sub_matrices(self.padded_input)
-        
-        batch_size, output_height, output_width, num_filters = self._get_output_shape(inputs)
-        output = np.empty((batch_size, output_height, output_width, num_filters), dtype=inputs.dtype)  
-        for p in self.params:
-            weights = p[:-1]
-            bias = p[-1]
-            weights = weights.reshape(self.kernel_size, self.kernel_size, self.input_channels)
-            convolved = np.tensordot(sub_matrices, weights, axes=([2, 3, 4], [0, 1, 2]))
-            convolved += bias
-            convolved = convolved.reshape(batch_size, output_height, output_width, 1)
-            output = np.dstack((output, convolved))
-
-        return output
-
-    def backward(self, prev_grad):
-        sub_matrices = self._get_conv_sub_matrices(self.padded_input)
-        # Calculate gradients for weights
-        for i, p in enumerate(self.params):
-            prev_grad_filter = prev_grad[:, :, :, i].squeeze()
-            grads = np.einsum('ijkl,ijk->ijkl', sub_matrices, prev_grad_filter)
-            
-
-
+    
     def _get_conv_sub_matrices(self, inputs):
         # Get the sub matrices for the convolution
-        item_size = inputs.item_size 
+        item_size = inputs.itemsize 
         batch_size = inputs.shape[0]
         input_height = inputs.shape[1]
         input_width = inputs.shape[2]
         input_channels = inputs.shape[3]
 
-        # 1. Get the shape of the sub matrices
+        # Get the shape of the sub matrices
         kernel_size = self.kernel_size
-        __, output_height, output_width, _ = self.get_output_shape(inputs)
-
+        __, output_height, output_width, _ = self._get_output_shape(inputs)
         shape = (batch_size, output_height, output_width, kernel_size, kernel_size, input_channels)
 
         # Get strides of the sub matrices
@@ -166,12 +206,74 @@ class Conv2D():
 
 class MaxPool2D():
 
-    def __init__(self, kernel_size, stride=1, padding=0):
+    def __init__(self, kernel_size):
         self.kernel_size = kernel_size
-        self.stride = stride
-        self.padding = padding
+        self.stride = kernel_size
         self.trainable = False
+        self.grads = None
 
+        # Variables during forward pass to be used for the backward pass
+        self.input = None
+        self.sub_matrices = None
+
+    def __call__(self, input):
+        self.input = input
+        sub_matrices = self._get_pool_sub_matrices(self.input)
+        self.sub_matrices = sub_matrices
+        batch_size, output_height, output_width, output_channels = self._get_output_shape(self.input)
+        # Reshape from (batch_size, num_sub_matrices, kernel_size, kernel_size, input_channels) to (batch_size * num_sub_matrices, kernel_size, kernel_size, input_channels)
+        sub_matrices = np.reshape(sub_matrices, (sub_matrices.shape[0] * sub_matrices.shape[1], sub_matrices.shape[2], sub_matrices.shape[3], sub_matrices.shape[4]))
+        # Get the max value for each sub matrix
+        max_pool = np.max(sub_matrices, axis=(1,2))
+        # Reshape from (batch_size * num_sub_matrices, input_channels) to (batch_size, output_height, output_width, output_channels)
+        # Output channels is the same as input channels for max pooling
+        output = np.reshape(max_pool, (batch_size, output_height, output_width, output_channels))
+        return output
+
+
+    def backward(self, prev_grad):
+        pass
+
+
+    def _get_output_shape(self, inputs):
+        batch_size = inputs.shape[0]
+        input_height = inputs.shape[1]
+        input_width = inputs.shape[2]
+        input_depth = inputs.shape[3]
+
+        output_width = int((input_width - self.kernel_size) / self.stride + 1)
+        output_height = int((input_height - self.kernel_size) / self.stride + 1)
+        return batch_size, output_height, output_width, input_depth
+    
+    def _get_pool_sub_matrices(self, inputs):
+        # Get the sub matrices for the pooling
+        item_size = inputs.itemsize 
+        batch_size = inputs.shape[0]
+        input_height = inputs.shape[1]
+        input_width = inputs.shape[2]
+        input_channels = inputs.shape[3]
+
+        # Get the shape of the sub matrices
+        kernel_size = self.kernel_size
+        __, output_height, output_width, _ = self._get_output_shape(inputs)
+        shape = (batch_size, output_height, output_width, kernel_size, kernel_size, input_channels)
+
+        # Get strides of the sub matrices
+        channel = item_size
+        column = channel * input_channels
+        row = column * input_width
+        horizontal_stride = self.stride * column
+        vertical_stride = self.stride * row
+        batch = input_height * input_width * column
+        stride_shape = (batch, vertical_stride, horizontal_stride, row, column, channel)
+
+        # Get the sub matrices
+        sub_matrices = np.lib.stride_tricks.as_strided(inputs, shape=shape, strides=stride_shape)
+
+        # Combine output height and output width
+        sub_matrices = np.reshape(sub_matrices, (batch_size, output_height * output_width, kernel_size, kernel_size, input_channels))
+
+        return sub_matrices
 
         
 class ReLU():
@@ -186,7 +288,6 @@ class ReLU():
 
     def backward(self, prev_grad):
         input_grads = prev_grad * (self.inputs > 0)
-        # return np.sum(grads, axis=0)
         return input_grads
     
 class MSE():
@@ -218,7 +319,5 @@ class SVM():
     
     def backward(self, prev_grad):
         input_grads = prev_grad * -self.targets * (self.inputs * self.targets < 1) / len(self.inputs)
-        # print(np.sum(grads, axis=0))
-        # return np.sum(grads, axis=0)
         return input_grads
     
--- code/tests/matrix/test.py
+++ code/tests/matrix/test.py
@@ -8,14 +8,12 @@ def test_FC():
     i = np.array([[1]])
     o = fc(i)[0]
     grad = fc.backward(np.array([[1, 2]]))
-    print(o)
-    print(grad)
     assert np.array_equal(o, np.array([4, 6]))
     assert np.array_equal(grad, np.array([3]))
     assert np.array_equal(fc.grads, np.array([[1, 2],[1, 2]]))
     assert np.array_equal(fc.parameters(), np.array([1, 2, 3, 4]))
 
-def test_net():
+def test_FC_net():
 
     class NN(Network):
         def __init__(self):
@@ -29,34 +27,101 @@ def test_net():
         def __call__(self, input):
             return self.forward(input)
         
-    
     # Instantiate the model
     model = NN()
     loss = SVM()
-
 
     # forward
     i = np.array([[0, 0], [1, 1]])
     o = np.array([[0], [0.1]])
     outputs = model(i)
-    print(outputs)
-#     [[0.]
-#      [2.]]
 
-    # backward
     # svm "max-margin" loss
     svm_loss = loss(outputs, o)
-    print(svm_loss)
-    # 0.9
+
+    # backward
     model.backward(loss)
-    print(model.parameters())
-    # (array([1., 1., 0.]), array([-0.05, -0.05, -0.05 ]))
 
-def test_convolutional_layer():
+    assert np.array_equal(outputs, np.array([[0], [2]]))
+    assert svm_loss == 0.9
+    assert np.array_equal(model.parameters()[0], np.array([1, 1, 0]))
+    assert np.array_equal(model.parameters()[1], np.array([-0.05, -0.05, -0.05]))
 
+def test_convolution():
+    inputs = np.array(
+        [
+            [
+                [[1], [1], [1]],
+                [[2], [2], [2]],
+                [[3], [3], [3]]
+            ]
+        ]
+    )
+    conv_output = np.array(
+        [
+            [
+                [[7], [7]],
+                [[11], [11]]
+            ]
+        ]
+    )
+    layer = Conv2D(input_channels=1, kernel_size=2, number_filters=1, stride=1, padding=0)
+    layer.params = np.array([[1, 1, 1, 1, 1]])
+    outputs = layer(inputs)
+
+    prev_gradients = np.array(
+        [
+            [
+                [[1], [1]],
+                [[2], [2]]
+            ]
+        ]
+    )
+    calculated_input_gradients = layer.backward(prev_gradients)
+    actual_input_gradients = np.array(
+        [
+            [
+                [[1], [2], [1]],
+                [[3], [6], [3]],
+                [[2], [4], [2]]
+            ]
+        ]
+    )
+    actual_gradients = np.array([[10, 10, 16, 16, 6]])
+
+
+    assert np.array_equal(actual_input_gradients, calculated_input_gradients)
+    assert np.array_equal(actual_gradients, layer.grads)
+    assert np.array_equal(outputs, conv_output)
+
+def test_max_pooling():
+    inputs = np.array(
+        [
+            [
+                [[1, 4], [1, 4], [1, 4], [1, 4]],
+                [[2, 3], [2, 3], [2, 3], [2, 3]],
+                [[3, 2], [3, 2], [3, 2], [3, 2]],
+                [[4, 1], [4, 1], [4, 1], [4, 1]]
+            ]
+        ]
+    )
+    layer = MaxPool2D(kernel_size=2)
+    outputs = layer(inputs)
+    prev_gradients = np.array(
+        [
+            [
+                [[1, 1], [1, 1]],
+                [[2, 2], [2, 2]]
+            ]
+        ]
+    )
+
+    calculated_input_gradients = layer.backward(prev_gradients)
     
+    assert np.array_equal(outputs, np.array([[[[2, 4], [2, 4]], [[4, 2], [4, 2]]]]))
 
 # test_FC()
-# test_net()
-test_convolutional_layer()
+# test_FC_net()
+# test_convolution()
+test_max_pooling()
     
