81755f34246e4a9db962e4cf302e4d3a5e976bfb
Sujay Garlanka <sujay.garlanka@gmail.com>
2023-12-09 13:35:11 -0800

    wip

--- code/flappy_bird/flappy_stable_baselines.py
+++ code/flappy_bird/flappy_stable_baselines.py
@@ -1,25 +1,46 @@
 import flappy_bird_gymnasium
 import gymnasium as gym
 from stable_baselines3.common.callbacks import EvalCallback
+from wandb.integration.sb3 import WandbCallback
+import wandb
+
+# model.learn(..., callback=WandbCallback())
 
 from stable_baselines3 import DQN, PPO
 
-# env = gym.make("FlappyBird-v0", render_mode="rgb_array")
+
+# eval_env = gym.make("FlappyBird-v0", render_mode="rgb_array")
 eval_env = gym.make("CartPole-v1", render_mode="rgb_array")
-eval_callback = EvalCallback(eval_env, n_eval_episodes=4, 
-                             best_model_save_path="./logs/best_model",
-                             log_path="./logs/results",
-                             deterministic=True, render=False)
 
 
-# model = DQN("MlpPolicy", env, verbose=1)
-# model.learn(total_timesteps=1000000, log_interval=4)
-# model.save("dqn_cartpole")
+# config = {
+#     "policy_type": "MlpPolicy",
+#     "total_timesteps": 100000,
+#     "env_id": "CartPole-v1",
+# }
 
+# run = wandb.init(
+#     project="cartpole-v1-stable-baselines3",
+#     config=config,
+#     sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
+#     # monitor_gym=True,  # auto-upload the videos of agents playing the game
+#     # save_code=True,  # optional
+# )
+
+eval_callback = EvalCallback(
+    eval_env,
+    deterministic=True,
+    render=False,
+)
 env = gym.make("CartPole-v1", render_mode="rgb_array")
 model = DQN("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=25000, callback=eval_callback, log_interval=4)
-model.save("dqn_cartpole")
+# model.learn(
+#     total_timesteps=100000, callback=WandbCallback(verbose=1, log="all"), log_interval=4
+# )
+model.learn(
+    total_timesteps=100000, callback=eval_callback, log_interval=4
+)
+# model.save("dqn_cartpole")
 
 # del model # remove to demonstrate saving and loading
 
